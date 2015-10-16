55547fab27aa09c04ce7d3f3c27f648af78aa68f
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2015-10-16 18:20:47 +0200

    general cleanup

--- bgg.game.parsing.R
+++ bgg.game.parsing.R
@@ -40,20 +40,6 @@ games.sitemap2 <- do.call(
 
 games.id <- as.character(do.call(rbind, lapply(strsplit(as.character(games.sitemap$loc), "/"), function(x) { x[5] })))
 
-bgg.complete.1 <- bgg.get(games.id[1:10000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.2 <- bgg.get(games.id[10001:20000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.3 <- bgg.get(games.id[20001:30000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.4 <- bgg.get(games.id[30001:40000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.5 <- bgg.get(games.id[40001:50000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.6 <- bgg.get(games.id[50001:60000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.7 <- bgg.get(games.id[60001:70000], .progress = create_progress_bar("text"))
-beep(8)
-bgg.complete.8 <- bgg.get(games.id[70001:79925], .progress = create_progress_bar("text"))
+bgg.complete <- bgg.get(games.id, .progress = create_progress_bar("text"))
 
 
