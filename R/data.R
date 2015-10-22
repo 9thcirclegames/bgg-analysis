9475453a8db3b372ea2f1940d5181ebf91d8cdef
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2015-10-22 15:44:22 +0200

    update dataset

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing the attributes and the ratings for around 80,000 boardgames
 #' in a format suitable for data mining.
 #'
-#' @format A data frame with 79916 rows and 98 variables
+#' @format A data frame with 79916 rows and 97 variables
 #' @docType data
 #' @keywords datasets
 #' @name BoardGames
