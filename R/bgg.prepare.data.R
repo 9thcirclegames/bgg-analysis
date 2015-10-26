d0edabd50b2f728a0775f702e4f3036d74edfbe6
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2015-10-26 23:19:57 +0100

    fix an issue with import clause for dplyr operator

--- R/bgg.prepare.data.R
+++ R/bgg.prepare.data.R
@@ -16,28 +16,28 @@
 #' @param bgg.dataset the BoardGames dataset object to prepare
 #'
 #' @importFrom splitstackshape cSplit_e
-#' @importFrom dplyr select
+#' @importFrom dplyr select "%>%"
 #'
 #' @export
 bgg.prepare.data <- function(bgg.dataset = BoardGames){
 
   # I really don't want videogames in my DB
   videogames.id <- unique(c(
-    which(!is.na(bgg.dataset$stats.family.amiga.pos)),
-    which(!is.na(bgg.dataset$stats.family.arcade.pos)),
-    which(!is.na(bgg.dataset$stats.family.atarist.pos)),
-    which(!is.na(bgg.dataset$stats.family.commodore64.pos))
+    which(!is.na(bgg.dataset$stats.rank.family.amiga.pos)),
+    which(!is.na(bgg.dataset$stats.rank.family.arcade.pos)),
+    which(!is.na(bgg.dataset$stats.rank.family.atarist.pos)),
+    which(!is.na(bgg.dataset$stats.rank.family.commodore64.pos))
   )
   )
 
   bgg.dataset <- bgg.dataset[-videogames.id,]
 
   bgg.dataset <- select(bgg.dataset, -one_of("details.image", "details.thumbnail", "details.description", "stats.median"))
-  bgg.dataset <- select(bgg.dataset, -contains("stats.family.amiga"),
-               -contains("stats.family.arcade"),
-               -contains("stats.family.atarist"),
-               -contains("stats.family.commodore64"),
-               -contains("stats.subtype.videogame")
+  bgg.dataset <- select(bgg.dataset, -contains("stats.rank.family.amiga"),
+               -contains("stats.rank.family.arcade"),
+               -contains("stats.rank.family.atarist"),
+               -contains("stats.rank.family.commodore64"),
+               -contains("stats.rank.subtype.videogame")
   )
 
   # Zero value in yearpublished column is a missing, so I convert to NA
@@ -47,7 +47,7 @@ bgg.prepare.data <- function(bgg.dataset = BoardGames){
   bgg.dataset[which(bgg.dataset$details.minage==0 | bgg.dataset$details.minage > 25),]$details.minage <- NA
 
   # I'm going to filter out games that were released before 1959 or not yet released
-  # We're going to lose only 5% of the dataset
+  # We're going to loose only 5% of the dataset
   summary(as.numeric(bgg.dataset$details.yearpublished))
   quantile(as.numeric(bgg.dataset$details.yearpublished), seq(0, 1, 0.05), na.rm = TRUE)
 
