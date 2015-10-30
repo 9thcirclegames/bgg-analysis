a9734cd8bdf1d86fb4cfb8e8efaa6190e6ceb490
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2015-10-30 01:32:44 +0100

    added some ranking analysis

--- vignettes/exploratory_analysis.R
+++ vignettes/exploratory_analysis.R
@@ -1,7 +1,12 @@
 data("BoardGames")
 
 BoardGames <- bgg.prepare.data(BoardGames)
-BoardGames.test <- BoardGames %>% filter(is.na(details.yearpublished) | details.yearpublished <= 2015)
+#########################################
+# PREFILTERING                          #
+#########################################
+bgg.useful <- BoardGames %>% filter(is.na(details.yearpublished) | details.yearpublished <= 2015) %>% filter(stats.usersrated >= 25, game.type == "boardgame")
+BoardGames.test <- BoardGames %>% filter(details.yearpublished > 2015) %>% filter(game.type == "boardgame")
+
 
 
 # summarize by year
@@ -43,10 +48,6 @@ ggplot(BoardGames, aes(stats.usersrated)) +
   ggtitle("Cumulative freq-plot of User Ratings") +
   xlim(0, quantile(BoardGames$stats.usersrated, seq(0, 1, 0.05), na.rm = TRUE)['95%'])
 
-# ---------> filter out games where ratings < 25
-
-bgg.useful <- BoardGames %>% filter(stats.usersrated >= 25)
-
 # Cumulative Freq-plot of average rating
 quantile(bgg.useful$stats.average, seq(0, 1, 0.05), na.rm = TRUE)
 
@@ -55,9 +56,54 @@ ggplot(bgg.useful, aes(stats.usersrated)) +
 ggtitle("Cumulative freq-plot of User Ratings") +
   xlim(c(0, quantile(bgg.useful$stats.usersrated, seq(0, 1, 0.05), na.rm = TRUE)['95%']))
 
-# Ratings are gaussian! Holy crap, this is cool (and unexpected)
+#########################################
+# PREPARING SOME TOP RANK ATTRIBUTES    #
+#########################################
+how.many.tops <- 7
+
+bgg.categories.freq <- (bgg.useful %>% group_by(attributes.boardgamecategory) %>% summarize(count=n()))
+bgg.categories.freq <- bgg.categories.freq[order(-bgg.categories.freq[,2]),]
+bgg.top.categories <- subset(bgg.useful, attributes.boardgamecategory %in% head(bgg.categories.freq$attributes.boardgamecategory, how.many.tops))
+bgg.worst.categories <- subset(bgg.useful, !(attributes.boardgamecategory %in% head(bgg.categories.freq$attributes.boardgamecategory, how.many.tops)))
+
+
+bgg.mechanics.freq <- (bgg.useful %>% group_by(attributes.boardgamemechanic) %>% summarize(count=n()))
+bgg.mechanics.freq <- bgg.mechanics.freq[order(-bgg.mechanics.freq[,2]),]
+bgg.top.mechanics <- subset(bgg.useful, attributes.boardgamemechanic %in% head(bgg.mechanics.freq$attributes.boardgamemechanic, how.many.tops))
+bgg.worst.mechanics <- subset(bgg.useful, !(attributes.boardgamemechanic %in% head(bgg.mechanics.freq$attributes.boardgamemechanic, how.many.tops)))
+
+#########################################
+# EXAMINING THE AVERAGE RATINGS         #
+#########################################
+
+# Ratings are gaussian
 ggplot(bgg.useful, aes(x = stats.average)) +
   geom_histogram(aes(y = ..density..), binwidth = .1, fill="red", alpha=.2, col="deeppink") + geom_density(col="red", lwd=1) +
+  xlim(0,10) +
+  geom_vline(xintercept=mean(bgg.useful$stats.average, na.rm=TRUE), color="black")
+
+# Still gaussian while splitting by language_dependence
+# Unplayable games seems to suffer a considerable rating loss
+ggplot(bgg.useful, aes(x = stats.average, colour=polls.language_dependence)) +
+  geom_density() +
+  geom_density(aes(x = stats.average), col="red", lwd=1, fill="deeppink", alpha=.2) +
+  xlim(0,10) +
+  geom_vline(xintercept=mean(bgg.useful$stats.average, na.rm=TRUE), color="black")
+
+# Some gamemechanics express a gaussian distribution with a different median than the cumulative distribution
+# game mechanics from the top could be a good classification attribute: Roll, Tile Placement, Hand Managements
+ggplot(bgg.useful %>% mutate(attributes.top.mechanics=ifelse(game.id %in% bgg.top.mechanics$game.id, attributes.boardgamemechanic, "Other Mechanic")),
+       aes(x = stats.average, colour=attributes.top.mechanics)) +
+  geom_density() +
+  geom_density(data=bgg.useful, aes(x = stats.average), col="red", lwd=1, fill="deeppink", alpha=.2) +
+  xlim(0,10) +
+  geom_vline(xintercept=mean(bgg.useful$stats.average, na.rm=TRUE), color="black")
+
+# Historical wargames have higher average rating, yet still gaussian distribution
+ggplot(bgg.useful %>% mutate(attributes.top.categories=ifelse(game.id %in% bgg.top.categories$game.id, attributes.boardgamecategory, "Other Category")),
+       aes(x = stats.average, colour=attributes.top.categories)) +
+  geom_density() +
+  geom_density(data=bgg.useful, aes(x = stats.average), col="red", lwd=1, fill="deeppink", alpha=.2) +
   xlim(0,10) +
   geom_vline(xintercept=mean(bgg.useful$stats.average, na.rm=TRUE), color="black")
 
@@ -101,7 +147,7 @@ ggplot(bgg.useful, aes(x = stats.owned)) +
   xlim(c(0, quantile(bgg.useful$stats.owned, seq(0, 1, 0.05), na.rm = TRUE)['95%']))
 
 ggplot(bgg.useful, aes(x = stats.usersrated)) +
-  geom_histogram(aes(y = ..density..), binwidth = 2, fill="red", alpha=.2, col="deeppink") + geom_density(col="red", lwd=1) +
+  geom_histogram(aes(y = ..density..), binwidth = 5, fill="red", alpha=.2, col="deeppink") + geom_density(col="red", lwd=1) +
   geom_vline(xintercept=median(bgg.useful$stats.owned, na.rm=TRUE), color="black") +
   xlim(c(0, quantile(bgg.useful$stats.usersrated, seq(0, 1, 0.05), na.rm = TRUE)['95%']))
 
@@ -115,35 +161,64 @@ ggplot(bgg.useful, aes(x = stats.averageweight)) +
 
 # la variabile dipendente è normale, tutte le statistiche di collection seguono la powerlaw
 
-# c'è una certa regressione positiva con la difficoltà e il rating:
-ggplot(bgg.useful[which(bgg.useful$stats.averageweight > 0),], aes(x=stats.averageweight, y=stats.average)) +
+##########################################
+# REGRESSION BETWEEN WEIGHT AND AVERAGE  #
+##########################################
+
+# There is an evident positive regression between weight and rating
+# This is due the fact that BGG users are almost hardcore gamers
+ggplot(bgg.useful %>% filter(stats.averageweight > 0), aes(x=stats.averageweight, y=stats.average)) +
   geom_point(alpha=.2, col="deeppink") +
-  geom_smooth(data=subset(bgg.useful,
-                          attributes.boardgamemechanic %in% head(bgg.mechanics.freq[ order(-bgg.mechanics.freq[,2]), ]$attributes.boardgamemechanic,5)),
-              aes(x=stats.averageweight, y=stats.average, color=attributes.boardgamemechanic),
-              method="lm",
-              se=FALSE,
-              fullrange=TRUE) +
-  geom_smooth(method="lm", lwd=1) +
+   geom_smooth(method="lm", lwd=1, fullrange=TRUE) +
   ylim(2.5, 9)
-# (hardcore gamers -> bisogna spaccare le dummy sulle categorie perché sicuramente su children, party ecc. l'andamento non è lo stesso)
 
-# slope = 0, ma non per tutti i language_dependence...e' sicuramente qualcosa legato alla meccanica
-ggplot(bgg.useful, aes(x=stats.trading, y=stats.average, color=polls.language_dependence)) +
-  geom_point(alpha=.2, col="deeppink") +
-  geom_smooth(method="lm", se=FALSE) +
-  xlim(c(0, quantile(bgg.useful$stats.trading, seq(0, 1, 0.05), na.rm = TRUE)['95%']))
+# For simpler mechanics and games, the positive regression between weight and rating is stronger than for complex games like hex-and-counter
+ggplot(bgg.useful %>% filter(stats.averageweight > 0) %>% mutate(attributes.top.mechanics=ifelse(game.id %in% bgg.top.mechanics$game.id, attributes.boardgamemechanic, "Other Mechanic")),
+       aes(x=stats.averageweight, y=stats.average, colour=factor(attributes.top.mechanics))) +
+  geom_point(alpha=.2) +
+  geom_smooth(method="lm", lwd=1, fullrange=TRUE) +
+  geom_smooth(data=bgg.useful %>% filter(stats.averageweight > 0),
+              aes(x=stats.averageweight, y=stats.average), method="lm", se=FALSE, col="grey") +
+  facet_grid(. ~ attributes.top.mechanics) +
+  ylim(2.5, 9)
 
-bgg.mechanics.freq <- bgg.useful %>% group_by(attributes.boardgamemechanic) %>% summarize(count=n())
+# It doens't seems to be an effect of language dependence between weight and rating
+ggplot(bgg.useful %>% filter(stats.averageweight > 0),
+       aes(x=stats.averageweight, y=stats.average, colour=polls.language_dependence)) +
+  geom_point(alpha=.2) +
+  geom_smooth(method="lm", lwd=1, fullrange=TRUE) +
+  geom_smooth(data=bgg.useful %>% filter(stats.averageweight > 0),
+              aes(x=stats.averageweight, y=stats.average), method="lm", se=FALSE, col="grey") +
+  facet_grid(. ~ polls.language_dependence) +
+  ylim(2.5, 9)
 
-# effettivamente e' cosi' principalmente per dice e hex...da approfondire)
-ggplot(bgg.useful, aes(x=stats.trading, y=stats.average, color=attributes.boardgamemechanic)) +
-  geom_point(alpha=.2, col="deeppink") +
-  geom_smooth(data=subset(bgg.useful,
-                          attributes.boardgamemechanic %in% head(bgg.mechanics.freq[ order(-bgg.mechanics.freq[,2]), ]$attributes.boardgamemechanic,10)), aes(x=stats.trading, y=stats.average, color=attributes.boardgamemechanic), method="lm", se=FALSE) +
-  xlim(c(0, quantile(bgg.useful$stats.trading, seq(0, 1, 0.05), na.rm = TRUE)['95%']))
+# The positive regression between weight and rating is less strong in wargames and complex categories
+ggplot(bgg.useful %>% filter(stats.averageweight > 0) %>% mutate(attributes.top.categories=ifelse(game.id %in% bgg.top.categories$game.id, attributes.boardgamecategory, "Other Category")),
+       aes(x=stats.averageweight, y=stats.average, colour=factor(attributes.top.categories))) +
+  geom_point(alpha=.2) +
+  geom_smooth(method="lm", lwd=1, fullrange=TRUE) +
+  geom_smooth(data=bgg.useful %>% filter(stats.averageweight > 0),
+              aes(x=stats.averageweight, y=stats.average), method="lm", se=FALSE, col="grey") +
+  facet_grid(. ~ attributes.top.categories) +
+  ylim(2.5, 9)
 
-bgg.category.freq <- bgg.useful %>% group_by(attributes.boardgamecategory) %>% summarize(count=n())
+#######################################
+# LANGUAGE DEPENDENCE                 #
+#######################################
+# As seen before, only unplayable games suffer a minor rating loss
+ggplot(bgg.useful %>% mutate(attributes.top.mechanics=ifelse(game.id %in% bgg.top.mechanics$game.id, attributes.boardgamemechanic, "Other Mechanic")),
+       aes(x=polls.language_dependence, y=stats.average, colour=polls.language_dependence)) +
+  geom_boxplot(alpha=.2) +
+  #facet_grid(. ~ attributes.top.mechanics)
+  ylim(2.5, 9)
+
+# Weird enough, unplayable games are rated as the most simpler.
+# I think that the "Unplayable status could have some severe issues (does the rater understand the meaning?)
+ggplot(bgg.useful %>% filter(stats.averageweight > 0) %>% mutate(attributes.top.mechanics=ifelse(game.id %in% bgg.top.mechanics$game.id, attributes.boardgamemechanic, "Other Mechanic")),
+       aes(x=polls.language_dependence, y=stats.averageweight, colour=polls.language_dependence)) +
+  geom_boxplot(alpha=.2) +
+  facet_grid(. ~ attributes.top.mechanics)
+  ylim(0, 5)
 
 #
 ggplot(bgg.useful, aes(x=stats.trading, y=stats.average, color=attributes.boardgamecategory)) +
